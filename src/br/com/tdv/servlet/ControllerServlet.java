f47b0f9be024aa309615c12f4ff83b6459447b3a
Diego Lirio <diegolirio.dl@gmail.com>
2014-01-08 18:19:55 -0200

    init project mvc

---
+++ src/br/com/tdv/servlet/ControllerServlet.java
@@ -0,0 +1,44 @@
+package br.com.tdv.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControllerServlet
+ */
+@WebServlet("/controller")
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControllerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+    	super.service(arg0, arg1);
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
